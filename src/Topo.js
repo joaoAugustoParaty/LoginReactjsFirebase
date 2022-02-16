5c8aa53c9b40775b2cda2ed17971a8b758885456
joaoAugustoParaty <joaodrow@gmail.com>
2022-02-16 00:14:53 -0300

    Instalando o React

---
+++ src/Topo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Topo (){
+    return (
+        <>
+        <div>
+            Topo do Site 
+            </div>
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Topo from './Topo';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Topo />
     <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
